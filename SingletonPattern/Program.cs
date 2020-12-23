1c601d3e8336a67d38ea79be623943f439e49989
VuongTrinh <vuongtrinh.dev@gmail.com>
2020-12-23 13:45:21 +0700

    Add Singleton Pattern.

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var chocoEggs = ChocolateBoiler.GetInstance();
+                chocoEggs.Fill();
+                chocoEggs.Boil();
+                chocoEggs.Drain();
+            }
+            catch (Exception)
+            {
+                Console.Write("Oops");
+            }
+        }
+    }
+}
